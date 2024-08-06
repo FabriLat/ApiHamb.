16ec6fdba35a6cfdce82602220d29a644de1b4a1
unknown <FabricioLattante722@gmail.com>
2024-08-06 16:35:25 -0300

    Entidades principales creadas

---
+++ src/Domain/Entities/Admin.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Admin : User
+    {
+
+        public Admin() { }
+
+        public Admin(string fullName, string email, string password) 
+        {
+            FullName = fullName;
+            Email = email;
+            Password = password;
+            UserType UserRole = UserType.Admin;
+        }
+    }
+}
